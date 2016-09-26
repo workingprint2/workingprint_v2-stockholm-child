3e2b7f7aa4964069684b8089985159411cb66e73
Workingprint2 <workingprint2@gmail.com>
2016-09-26 19:07:42 -0400

    functions changes

--- functions.php
+++ functions.php
@@ -7,3 +7,19 @@ wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  
 wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+
+//
+
+
+// Adds page name to body class
+function body_class_names($classes) {
+global $post;
+$name = $post->post_name;
+$classes[] = $name;
+return $classes;
+}
+
+add_filter('body_class','body_class_names');
+
+?>
